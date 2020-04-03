ec76a14e22c41bdaaebb8bd6bcbf618bceb9b6cc
David Tran <davidttemail@gmail.com>
2020-04-03 10:50:41 -0600

    Add MVC for adding a ride to a mechanic

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -7,4 +7,10 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:mechanic_id])
   end
+
+  def add_ride
+    mechanic = Mechanic.find(params[:mechanic_id])
+    RideMechanic.create(mechanic_id: mechanic.id, ride_id: params[:ride_id])
+    redirect_to "/mechanics/#{mechanic.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "/mechanics", to: "mechanics#index"
   get "/mechanics/:mechanic_id", to: "mechanics#show"
+  put "/mechanics/:mechanic_id", to: "mechanics#add_ride"
 
   get "/amusement-parks/:park_id", to: "amusement_parks#show"
 end
