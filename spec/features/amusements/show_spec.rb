75e986e0c1efc6a90819cfe6637f4c106ee5ac42
David Tran <davidttemail@gmail.com>
2020-04-03 09:58:16 -0600

    Fix typo

--- spec/features/amusements/show_spec.rb
+++ spec/features/amusements/show_spec.rb
@@ -8,14 +8,14 @@ describe "amusments park show page" do
     ride_2 = park_1.rides.create({name: "Storm Runner", thrill: 9})
     ride_3 = park_1.rides.create({name: "The Greate Bear", thrill: 7})
 
-    visit "/amusement_park/#{park_1.id}"
+    visit "/amusement-park/#{park_1.id}"
 
     expect(page).to have_content(park_1.name)
-    expect(page).to have_content("Admissions: #{park_1.admission}")
+    expect(page).to have_content("Admissions: $#{park_1.admission}")
     expect(page).to have_content(ride_1.name)
     expect(page).to have_content(ride_2.name)
     expect(page).to have_content(ride_3.name)
-    expect(page).to have_content("Average Trill Rating of Rides: 7.0/10")
+    expect(page).to have_content("Average Thrill Rating of Rides: 7.0/10")
 
   end
 
