6bfb4398179f8e60fac9f0a1cb5151486e3f9853
David Tran <davidttemail@gmail.com>
2020-04-03 09:57:36 -0600

    Write test for averaging thrill rating of rides

--- spec/models/amusement-park_spec.rb
+++ spec/models/amusement-park_spec.rb
@@ -4,4 +4,16 @@ describe AmusementPark do
   describe "relationships" do
     it { should have_many :rides }
   end
+
+  describe "methods" do
+    it "returns average thrill rating of rides" do
+
+      park_1 = AmusementPark.create({name: "Hersey Park", admission: 50.00})
+      ride_1 = park_1.rides.create({name: "Lightning Racer", thrill: 5})
+      ride_2 = park_1.rides.create({name: "Storm Runner", thrill: 3})
+
+      expect(park_1.average_thrill_rating).to eq(4.0)
+
+    end
+  end
 end
