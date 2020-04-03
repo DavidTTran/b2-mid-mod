a4380a91a9566e27c1c3ab4c19e65214af669c90
David Tran <davidttemail@gmail.com>
2020-04-03 09:18:40 -0600

    Create DB and mechanics table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+mechanic_1 = Mechanic.create({name: "David Tran", years_of_exp: 10})
+mechanic_2 = Mechanic.create({name: "Test Buddy", years_of_exp: 5})
